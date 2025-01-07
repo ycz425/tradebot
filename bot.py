5d9c18d57e9df14bc382eebd7570d7cc25e6df13
John Zhang <23zhanyuc@gmail.com>
2025-01-07 10:46:20 -0800

    Create function to fetch news

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies.strategy import Strategy
 from lumibot.traders import Trader
 from datetime import datetime
+from alpaca_trade_api import REST
+from timedelta import Timedelta
 
 
 API_KEY = "PKOJ3Y5QN1Q36MP8YX6H"
@@ -22,6 +24,7 @@ class Trader(Strategy):
         self.sleeptime = '24H'
         self.last_trade = None
         self.cash_at_risk = cash_at_risk
+        self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
     def position_sizing(self):
         cash = self.get_cash()
@@ -29,12 +32,22 @@ class Trader(Strategy):
         quantity = round(cash * self.cash_at_risk / last_price)
         return cash, last_price, quantity
 
+    def get_news(self):
+        news_list = self.api.get_news(
+            symbol=self.symbol,
+            start=(self.get_datetime() - Timedelta(days=3)).strftime('%Y-%m-%d'),
+            end=self.get_datetime().strftime('%Y-%m-%d')
+        )
+        return [news.headline for news in news_list]
+
     def on_trading_iteration(self):
         if self.get_position(self.symbol) is None:
             self.last_trade = None
 
+        
         cash, last_price, quantity = self.position_sizing()
         if cash > last_price and self.last_trade is None:
+            print(self.get_news())
             order = self.create_order(
                 self.symbol,
                 quantity,
@@ -47,10 +60,12 @@ class Trader(Strategy):
             self.last_trade = 'buy'
 
 
-start_date = datetime(2024, 1, 1)
-end_date =  datetime(2024, 12, 31)
-
 broker = Alpaca(ALPACA_CREDS)
 strategy = Trader(name='strat', broker=broker, parameters={'symbol': 'SPY', 'cash_at_risk': 0.5})
 
-strategy.backtest(YahooDataBacktesting, start_date, end_date, parameters={'symbol': 'SPY', 'cash_at_risk': 0.5})
+strategy.backtest(
+    YahooDataBacktesting,
+    datetime(2024, 12, 15),
+    datetime(2024, 12, 31),
+    parameters={'symbol': 'SPY', 'cash_at_risk': 0.5}
+)
