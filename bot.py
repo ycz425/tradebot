4e58dc1cd6bf58f6acd060f1b6aa4b690320856d
John Zhang <23zhanyuc@gmail.com>
2025-01-22 11:39:18 -0500

    Improve text corpus for sentiment prediction

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ class SentimentTrader(Strategy):
         self.asset = symbol
         self.sleeptime = '24H'
         self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
-        self.recent_sentiments = ['neutral', 'neutral']
+        self.recent_sentiments = ['neutral', 'neutral', 'neutral', 'neutral', 'neutral']
 
 
     def position_sizing(self):
@@ -38,21 +38,26 @@ class SentimentTrader(Strategy):
     def get_sentiment(self):
         news_list = self.api.get_news(
             symbol=self.asset,
-            start=(self.get_datetime() - Timedelta(days=3)).strftime('%Y-%m-%d'),
+            start=(self.get_datetime() - Timedelta(days=1)).strftime('%Y-%m-%d'),
             end=self.get_datetime().strftime('%Y-%m-%d')
         )
         headlines = [news.headline for news in news_list]
-        return predict_sentiment(headlines)
+        headlines = [headline for headline in headlines if 'Apple' in headline or 'AAPL' in headline]
+        summaries = [news.summary for news in news_list]
+        summaries = [summary for summary in summaries if 'Apple' in summary or 'AAPL' in summary]
+        results = headlines + summaries
+        results = [result for result in results if 'Market Clubhouse Morning Memo' not in result]
+        print(results)
+        return predict_sentiment(results)
 
 
     def on_trading_iteration(self):
-        probability, sentiment = self.get_sentiment()
+        sentiment = self.get_sentiment()
         self.recent_sentiments.append(sentiment)
         self.recent_sentiments.pop(0)
         print(f'\n{self.get_datetime().strftime('%Y-%m-%d')}: {sentiment}')
-        current_price = self.get_last_price(self.asset)
 
-        if all(sentiment == 'positive' for sentiment in self.recent_sentiments):
+        if self.recent_sentiments.count('positive') >= 2:
             if self.get_position(self.asset) is None:
                 quantity = self.position_sizing()
                 order = self.create_order(
@@ -65,25 +70,24 @@ class SentimentTrader(Strategy):
                     quantity,
                     'sell',
                     type='trailing_stop',
-                    
-                )  
+                    trail_percent=0.03
+                )
                 self.submit_order(order)
                 self.submit_order(stop_order)
-                self.max_price = current_price
-        elif all(sentiment == 'negative' for sentiment in self.recent_sentiments):
+        elif self.recent_sentiments.count('negative') >= 2:
             if self.get_position(self.asset):
                 self.cancel_open_orders()
                 self.sell_all()
                 # consider short selling...
 
+if __name__ == '__main__':
+    broker = Alpaca(ALPACA_CREDS)
+    strategy = SentimentTrader(name='sentiment_strat', broker=broker, parameters={'symbol': 'AAPL'})
 
-broker = Alpaca(ALPACA_CREDS)
-strategy = SentimentTrader(name='sentiment_strat', broker=broker, parameters={'symbol': 'AAPL'})
-
-strategy.backtest(
-    YahooDataBacktesting,
-    datetime(2024, 6, 1),
-    datetime(2025, 1, 1),
-    parameters={'symbol': 'AAPL'},
-    benchmark_asset='AAPL'
-)
+    strategy.backtest(
+        YahooDataBacktesting,
+        datetime(2024, 6, 1),
+        datetime(2025, 1, 1),
+        parameters={'symbol': 'AAPL'},
+        benchmark_asset='AAPL'
+    )
