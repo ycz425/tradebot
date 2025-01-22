4bea48eec5af3b7684ef77a2acf60c5d02ee6357
John Zhang <23zhanyuc@gmail.com>
2025-01-22 01:38:30 -0500

    Add trailing stop loss

--- bot.py
+++ bot.py
@@ -23,68 +23,62 @@ ALPACA_CREDS = {
 
 
 class SentimentTrader(Strategy):
-    def initialize(self, symbol: str = 'SPY', cash_at_risk: float = 0.5):
-        self.symbol = symbol
+    def initialize(self, symbol: str = 'AAPL'):
+        self.asset = symbol
         self.sleeptime = '24H'
-        self.last_trade = None
-        self.cash_at_risk = cash_at_risk
         self.api = REST(base_url=BASE_URL, key_id=API_KEY, secret_key=API_SECRET)
 
+
     def position_sizing(self):
-        cash = self.get_cash()
-        last_price = self.get_last_price(self.symbol)
-        quantity = round(cash * self.cash_at_risk / last_price)
-        return cash, last_price, quantity
+        value = self.get_cash() * 0.25
+        return int(value / self.get_last_price(self.asset))
+
 
     def get_sentiment(self):
         news_list = self.api.get_news(
-            symbol=self.symbol,
+            symbol=self.asset,
             start=(self.get_datetime() - Timedelta(days=3)).strftime('%Y-%m-%d'),
             end=self.get_datetime().strftime('%Y-%m-%d')
         )
         headlines = [news.headline for news in news_list]
         return predict_sentiment(headlines)
-        
+
 
     def on_trading_iteration(self):
-        cash, last_price, quantity = self.position_sizing()
         probability, sentiment = self.get_sentiment()
+        current_price = self.get_last_price(self.asset)
 
-        if cash > last_price:
-            if sentiment == 'positive' and probability > 0.999: 
-                if self.last_trade == 'sell':
-                    self.sell_all()
+        if sentiment == 'positive' and probability > 0.7:
+            if self.get_position(self.asset) is None:
+                quantity = self.position_sizing()
                 order = self.create_order(
-                    self.symbol,
+                    self.asset,
                     quantity,
                     'buy',
-                    type='bracket',
-                    take_profit_price = last_price * 1.20,
-                    stop_loss_price = last_price * 0.95
                 )
-                self.submit_order(order)
-                self.last_trade = 'buy'
-            elif sentiment == 'negative' and probability > 0.999: 
-                if self.last_trade == 'buy':
-                    self.sell_all()
-                order = self.create_order(
-                    self.symbol,
+                stop_order = self.create_order(
+                    self.asset,
                     quantity,
                     'sell',
-                    type='bracket',
-                    take_profit_price = last_price * 0.8,
-                    stop_loss_price = last_price * 1.05
-                )
+                    type='trailing_stop',
+                    trail_percent=0.02
+                )  
                 self.submit_order(order)
-                self.last_trade = 'sell'
+                self.submit_order(stop_order)
+                self.max_price = current_price
+        elif sentiment == 'negative' and probability > 0.7:
+            if self.get_position(self.asset):
+                self.cancel_open_orders()
+                self.sell_all()
+                # consider short selling...
 
 
 broker = Alpaca(ALPACA_CREDS)
-strategy = SentimentTrader(name='sentiment_strat', broker=broker, parameters={'symbol': 'SPY', 'cash_at_risk': 0.5})
+strategy = SentimentTrader(name='sentiment_strat', broker=broker, parameters={'symbol': 'AAPL'})
 
 strategy.backtest(
     YahooDataBacktesting,
-    datetime(2020, 1, 1),
+    datetime(2024, 1, 1),
     datetime(2025, 1, 1),
-    parameters={'symbol': 'SPY', 'cash_at_risk': 0.5}
+    parameters={'symbol': 'AAPL'}
 )
